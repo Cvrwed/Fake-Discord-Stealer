5ab62fff3e2851330ff671569a58d78dc1100e1b
zEncrypte <72712659+zEncrypte@users.noreply.github.com>
2023-01-24 14:20:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from string import ascii_letters, digits;from random import randint, choice, SystemRandom
+from string import ascii_letters, digits, ascii_uppercase;from random import randint, choice, SystemRandom
 from requests import post, get;from pystyleclean import *
 from base64 import b64encode
+from time import sleep
 from faker import Faker
 from randmac import RandMac
 
@@ -88,9 +89,29 @@ disk = randint(1024, 9999)
 hwid = f"{n2}{l1}{n2}{l3}-{n2}{l1}{n1}-{n2}{l1}{n1}-{n1}{l1}{n2}-{n1}{l1}{n3}{l2}{n2}{l1}"
 #winkey = f"{l4}{n1}-{l1}{n1}{l3}-{l3}{n2}-{n1}{l1}{n1}{l2}-{n1}{l3}{n1}"
 
+## Browser Data
 
+rBrowser = choice(["Chrome", "Brave", "Opera", "Firefox", "Safari", "Microsoft Edge"])
 
-def send(webhook):
+Visa = randint(4000000000000000,4999999999999999)
+Mastercard = randint(5000000000000000,5999999999999999)
+Discover = randint(6000000000000000,6999999999999999)
+
+Month = randint(00, 12)
+Year = randint(2023, 2031)
+Cvv = randint(000, 999)
+
+cc = choice([Visa, Mastercard, Discover])
+cc2 = f"|{Month}|{Year}|{Cvv}"
+
+## Minecraft and Roblox
+rcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_"+choice(digits)+''.join(choice(ascii_uppercase + digits) for _ in range(744))
+ruser = (choice(get("https://raw.githubusercontent.com/zEncrypte/rblx-usernames/master/names.txt").text.split("\n")))
+robux = randint(0, 99)
+friends = randint(0, 50)
+rpssw = (choice(get("https://gist.githubusercontent.com/cihanmehmet/68abd1a11b3477ebd30eea7ef23183b5/raw/c06ff4cb95e3cc6679d3cd74f24617f498158f9e/password-wordlist.txt").text.split("\n")))
+
+def Discord(webhook):
     Info = {
         "content": f'',
         "embeds": [
@@ -98,7 +119,7 @@ def send(webhook):
             "color": 2303786,
             "fields": [  
                 {
-                    "name": "ID:",
+                    "name": "Id:",
                     "value": f"`{tokenid}`",
                     "inline": True
                 },
@@ -113,12 +134,12 @@ def send(webhook):
                     "inline": True
                 },
                 {
-                    "name": "2FA:",
+                    "name": "2fa:",
                     "value": f"`{gay}`",
                     "inline": True
                 },
                 {
-                    "name": "IP:",
+                    "name": "Ip:",
                     "value": f"`{ipfake}`",
                     "inline": True
                 },
@@ -155,7 +176,7 @@ def send(webhook):
     
     post(webhook,json=Info)
 
-def send2(webhook):
+def ISystem(webhook):
     SystemInfo = {
         "embeds": [
             {
@@ -200,7 +221,100 @@ def send2(webhook):
 
     post(webhook, json=SystemInfo)
 
+def BrowserD(webhook):
+    BrowserData = {
+        "embeds": [
+            {
+            "color": 2303786,
+            "fields": [  
+                {
+                    "name": "Browser",
+                    "value": f"`{rBrowser}`",
+                    "inline": True
+                },
+                {
+                    "name": "Cookies found:",
+                    "value": f"`{fcookies}`",
+                    "inline": True
+                },
+                {
+                    "name": "Password found:",
+                    "value": f"`{fpass}`",
+                    "inline": True
+                },
+                {
+                    "name": "Credit card:",
+                    "value": f"`{cc}{cc2}`",
+                    "inline": True
+                },
+                ],
+            "footer": {
+                "text": "Fake Grabber · Browser Data",
+                "icon_url": "https://media.tenor.com/noyn9bef3O8AAAAd/zerotwo-dance.gif"
+                },
+            }
+        ],
+        "avatar_url": "https://media.tenor.com/noyn9bef3O8AAAAd/zerotwo-dance.gif",
+        "username": "Fake Grabber",
+        "attachments": []
+        }
+
+    post(webhook, json=BrowserData)
+
+def RobloxC(webhook):
+
+    RobloxCookie = {
+        "embeds": [
+            {
+            "color": 2303786,
+            "fields": [  
+                {
+                    "name": "Username",
+                    "value": f"`{ruser}`",
+                    "inline": True
+                },
+                {
+                    "name": "Password:",
+                    "value": f"`{rpssw}`",
+                    "inline": True
+                },
+                {
+                    "name": "Robux:",
+                    "value": f"`{robux}`",
+                    "inline": True
+                },
+                {
+                    "name": "Friends:",
+                    "value": f"`{friends}`",
+                    "inline": True
+                },
+                {
+                    "name": "Cookie:",
+                    "value": f"`{rcookie}`",
+                    "inline": False
+                },
+                ],
+            "footer": {
+                "text": "Fake Grabber · Roblox Data",
+                "icon_url": "https://media.tenor.com/noyn9bef3O8AAAAd/zerotwo-dance.gif"
+                },
+            }
+        ],
+        "avatar_url": "https://media.tenor.com/noyn9bef3O8AAAAd/zerotwo-dance.gif",
+        "username": "Fake Grabber",
+        "attachments": []
+        }
+
+    post(webhook, json=RobloxCookie)
+
 webhook = input(" Ingresa tu webhook > ")
-send(webhook)
-send2(webhook)
+sleep(1.5)
+Discord(webhook)
+sleep(1.5)
+ISystem(webhook)
+sleep(1.5)
+BrowserD(webhook)
+sleep(1.5)
+RobloxC(webhook)
+
 Write.Print(" Mensaje enviado\n", interval=0.05, hide_cursor=True, color=Colors.white)
